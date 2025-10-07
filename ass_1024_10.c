304a7b0a899b5bec5280b915edf5a02d8d9f42dd
anvesha1328 <anvasamistry@gmail.com>
2025-10-07 16:15:43 +0530

    Update ass_1024_10.c

--- ass_1024_10.c
+++ ass_1024_10.c
@@ -1,36 +1,23 @@
 #include<stdio.h>
-
-
 int main()
-
-
 {
-
-
 int num,i,temp=1;
 
-
 printf("Enter the number:-");
-
 
 scanf("%d",&num);
 
 for(i=1;i<=num;i++)
 
-
 {
-
 
     printf("\n%d",i);
 
-
     temp=temp*i;
-
 
 }
 
 printf("\nFactorial = %d",temp);
-
 
 return 0;
 
