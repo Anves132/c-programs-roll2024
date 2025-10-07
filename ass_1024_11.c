a35abdffdc3673f8aead972ce07e764a2b3656c3
anvesha1328 <anvasamistry@gmail.com>
2025-10-07 16:17:15 +0530

    Update ass_1024_11.c

--- ass_1024_11.c
+++ ass_1024_11.c
@@ -1,52 +1,31 @@
 #include<stdio.h>
-
-
 #include<string.h>
-
-
 int main()
-
-
 {
-
-
 char str[234];
-
 
 int i,count=1;
 
 printf("Enter the word :-");
 
-
 fgets(str,sizeof(str),stdin);
-
 
 for(i=0;str[i]!='\0';i++)
 
-
 {
-
 
     if(str[i]==' ' &&str[i+1]!=' '&&str[i+1]!='\0')
 
-
 {
-
 
 count++;
 
-
 }
-
-
 }
-
 
 printf("Number of Words  = %d",count);
 
-
 return 0;
-
 
 }
 
